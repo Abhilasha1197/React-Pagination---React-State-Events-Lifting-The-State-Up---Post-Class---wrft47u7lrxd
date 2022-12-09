938d5014331e5a017d3313c68855afd343421bd5
abhilasha <abhilasha15597@gmail.com>
2022-12-09 16:50:32 +0530

    new

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,8 +1,18 @@
 import React from 'react'
-const PaginationButtonsList = () => {
+const PaginationButtonsList = ({activePage,setAtivePage}) => {
+    const buttons = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
     return (
         <div className="pagination-buttons-list">
+           {buttons.map((pn) =>{
+            return (
+                <buttons id = {`button-${pn}`} key={pn} className = {pn=== activePage? "active-button" : ""}
+                onClick = {() => {
+                    setAtivePage(pn)
+                }}
+                >PN-{pn}</buttons>
+            )
+           })}
 
         </div>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,9 @@
 import React from 'react'
-const Post = () => {
+const Post = ({heading,body}) => {
     return (
         <div className="post">
+            <h3>{heading}</h3>
+            <p>{body}</p>
 
         </div>
     )
